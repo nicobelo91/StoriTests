abde46c0b61a00c8344417f73c4a2209a89e38ac
Nicolas Cobelo <>
2024-06-28 19:48:09 -0300

    Create Xcode project

---
+++ StoriTests/StoriTests/StoriTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoriTestsApp.swift
+//  StoriTests
+//
+//  Created by Nicolas Cobelo on 28/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct StoriTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
